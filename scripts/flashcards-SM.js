1b9d294303842a0b51149df44a48a74c5cec89bf
Lars-dh <dhauwer.lars@hotmail.com>
2026-05-25 22:39:21 +0200

    Extended the flashcards some more
    Added flashcards
    Added css for keybindings tag
    Edited path to flashcards in index file

--- scripts/flashcards-SM.js
+++ scripts/flashcards-SM.js
@@ -153,7 +153,85 @@ const termsWin = [
     ["Scenario - Blue Screen Of Death BSOD", "Start op in <b>Safe Mode</b> via de <b>Startup Settings</b>, verwijder of update drivers die mogelijke problemen veroorzaken.<br>Als dat niet werkt: gebruik <b>Systeemherstel</b> in WinRE."],
     ["Scenario - Problemen met Secure Boot", "Schakel <b>Secure Boot</b> tijdelijk uit in de <b>BIOS/UEFI</b> instellingen om te controleren of dit het probleem veroorzaakt.<br>Als dat niet werkt: onderzoek de compabiliteit van de hardware en software met <b>Secure Boot</b>"],
     ["Scenario - GPT-schijf start niet op", "Zorg ervoor dat de computer in <b>UEFI-modus</b> opstart. Controleer of de <b>EFI-partitie</b> aanwezig en correct is.<br>Als dat niet werkt: gebruik <b>bootrec /RebuildBCD</b> in de <b>Command Prompt</b> van WinRE."],
-    []
+    ["VMware Workstation pro 17 - Stappenplan aanmaken VM met Windows 11 ISO", `Virutele Machine, of VM, aanmaken. Voorbereidingen en stappenplan:<br>Virtualisatie aanzetten in UEFI:<ul><li>Zoek naar de optie '<b>Virtualization Technology</b>' en schakel deze in ('enabled'). Deze staat meestal onder 'Advanced options'.</li></ul>Bereid de ISO-file voor:<ul><li><a href="https://www.microsoft.com/nl-nl/software-download/windows11">Download Windows11 ISO</a>.</li><li>Deze ISO vereist minstens <b>16 Gb RAM</b> en <b>40GB vrije opslagruimt</b>!</li><li>Het is aangeraden om op de C:-schijf een map voor alle ISO's aan te maken. Vb: VIRTUAL_ISO.</li></ul>Installeer de VM-software:<ul><li><a href="https://support.broadcom.com/group/ecx/productdownloads?subfamily=VMware%20Workstation%20Pro&freeDownloads=true">Download Workstation Pro 17</a>, kies jouw besturingssysteem. Deze link vereist een login, als Vives student kan je de software via de <a href="https://www.academicsoftware.eu/login">Academic Software</a> (inloggen met vives e-mail) verkrijgen door in de tab 'Software' te zoeken naar 'VMware Workstation Pro' en de stappen te volgen.</li></ul>Aanmaken van de Virutele Machine:<ol><li>Creëer mappen op de C:-schijf waar je de VM's en virtuele opslag zal opslaan. Vb: VIRTUAL_MACHINES en VIRTUAL_HARDDISKS.</li><li>Ga naar 'Edit' &rAarr; 'Preferences', pas hier de standaard map aan waar de virtuele machines bewaard worden.</li><li>Open de VMware Workstation Pro software en klik op '<b>Create a New Virtual Machine</b>'.</li><li>Kies voor '<b>Custom (advanced)</b>' in de Wizard, klik op 'Next'.</li>
+        <li>Laat de standaard keuze staan in het volgende scherm, <b>klik op 'Next'</b>.</li>
+        <li><b>Geef</b> de virtuele machine een passende <b>naam</b>. Vb: PARENT_VM (om later te clonen en alle settings die we moeten doorlopen niet steeds te herhalen).</li>
+        <li>Klik op '<b>Browse...</b>', navigeer naar en selecteer de ISO-file, klik op 'Next'.</li>
+        <li>Indien stap 2 niet uitgevoerd werd, klik op '<b>Browse...</b>' en navigeer naar de gewenste map, klik op 'Next'.</li>
+        <li>Laat de standaard keuze staan en <b>kies</b> een wachtwoord van minstens 8 tekens, klik op 'Next'.</li>
+        <li>Vink '<b>Secure Boot</b>' aan, klik op 'Next'.</li>
+        <li><b>Verhoog</b> het aantal cores per processor naar 4, klik op 'Next'.</li>
+        <li>We <b>behouden</b> de huidige selectie voor RAM geheugen, klik op 'Next'.</li>
+        <li>Bij '<b>Network Type</b>' kiezen we voor '<b>bridged</b> networking', klik op 'Next'.</li>
+        <li>In het volgende scherm ga door, klik op 'Next'.</li>
+        <li>Kies '<b>NVMe</b>' als Disk Type, klik op 'Next'.</li>
+        <li>Kies voor '<b>Create a new virtual disk</b>', klik op 'Next'.</li>
+        <li>Selecteer de gewenste virtual disk grootte.</li>
+        <li>Vink '<b>Allocate all disk space now</b>' ook aan (dit staat niet zo in de cursus). Indien dit niet aangevinkt staat kan dit later voor problemen zorgen:
+        <ul><li>Stel je vinkt dit niet aan en je hebt nog 80GB vrij op schijfruimte van de host. Je stelt de disk space van de VM in op 64GB.</li>
+        <li>Je gebruikt de VM en neemt met deze VM 30GB aan schijfruimte in. Je host heeft nu maar 50GB meer.</li>
+        <li>Je gebruikt de host en de beschikbare schijfruimte daalt naar 5GB.</li>
+        <li>Je VM denkt nog steeds dat er 34GB vrij is, maar dit is niet zo. Als je nu op de VM bestanden plaatst die meer dan 5GB innemen, zal je in de problemen komen.</li>
+        <li>Vink daarom dit dus aan!</li></ul></li>
+        <li>Selecteer ook de optie '<b>Store virtual disk as single file</b>', klik op 'Next'.</li>
+        <li>Klik op <b>Browse...</b> en navigeer naar de map die we in stap 1 gemaakt hebben om de virtual disk te bewaren, klik op 'Next'.</li>
+        <li>Finale stap, laatste kans om hardware nog te wijzigen, klik op 'Finish'.</li></ol>`],
+    ["VMware Workstation pro 17 - Windows installatie", `Windows11 installeren op de VM:
+        <ol>
+        <li><b>Start</b> de virtuele machine op en open de console. (Deze stap volgt meteen op het aanmaken van de VM en kan genegeerd worden.)</li>
+        <li><b>Klik</b> snel in het scherm en druk op een toest wanneer dit geprompt wordt.</li>
+        <li><b>Kies</b> de gewenste taal instellingen, let op: indien hier engels qwerty gekozen wordt kan dit aangepast worden tot op het bureaublad.</li>
+        <li><b>Wacht</b> tot Windows begint met de installatie wizard.</li>
+        <li><b>Druk</b> shift+F10 om de command prompt te openen.</li>
+        <li><b>Geef</b> het commando <b>oobe\BypassNRO</b> (hoofdletters!) in.</li>
+        <li>Terwijl de VM restart, <b>rechtermuis-klik</b> in het Workstation op de VM en kies '<b>Settings</b>'.</li>
+        <li><b>Navigeer</b> naar 'networkadapter' en vink 'connected' uit.</li>
+        <li><b>Volg</b> de installatie wizard en kies voor 'ik heb geen internet' bij de netwerkopties.</li>
+        <li><b>Geef</b> de gebruiker een naam.</li>
+        <li>Windows11 is gebruiksklaar.</li>
+        </ol>
+        Het is aangeraden om de VMware Tools te downloaden. Dit kan door in het Workstation te rechtermuis-klikken op de VM en te kiezen voor 'Install VMware Tools...'. Nadien open je de Verkenner in de VM en klik je op 'Deze PC'. Dubbelkik dan op de VMware Tools (D: schijf) en volg de installatie-wizard, kies voor 'Complete'. Na installatie verdwijnt de VMware Tools installer als D: schijf.`],
+    ["Soorten Windows Updates", `Elke soort update heeft een doel. Ze worden beheerd via 'Windows Updates', hier kunnen voorkeuren beheerd worden en hoe en wanneer updates worden uitgevoerd ingesteld.
+        <ul>
+        <li><b>Kwaliteitsupdates</b>: Maandelijkse updates die verbeteringen brengen in de beveiliging, betrouwbaarheid en prestaties van het OS. Ze bevatten vaak beveilingspatches en bugfixes.</li>
+        <li><b>Functie-updates</b>: Jaarlijkse updates die nieuwe functies, verbeteringen en wijzigingen in de GUI bevatten. Ze verrijken de gebruikservaring door nieuwe mogelijkheden toe te voegen.</li>
+        <li><b>Dirverupdates</b>: Verbeteren compabiliteit met hardware door de stuurprogramma's voor verschillende apparaten op een systeem bij te werken. Dit kan prestaties en stabiliteit van apparaten verbeteren.</li>
+        <li><b>Definitie-updates</b>: Houdt de virusdefinities van Windows Defender Antivirus up-to-date. Ze zorgen ervoor dat de pc bescherm is tegen de nieuwste dreigingen.</li>
+        <li><b>Optionele updates</b>: Niet cruciale verbeteringen of nieuwe functies die de gebruiker naar eigen wil kan installeren.</li>
+        </ul>`],
+    ["Windows Update raadplegen", `De Windows Update kan op volgende manier geraadpleegd worden:
+        <ul>
+        <li><b>Ga</b> naar '<b>Instellingen</b>' (<kbd>Ctrl</kbd> + <kbd>i</kbd>)</li>
+        <li><b>Update & Beveiliging</b></li>
+        <li><b>Windows Update</b></li>
+        </ul>`],
+    ["Drivers updaten", `Drivers updaten kan doormiddel van de ingebouwde Windows-tools:
+        <ul>
+        <li>Open <b>Apparaatbeheer</b> via het Start-menu.</li>
+        <li>OF rechtermuis-klik op het windows-icoon en kies '<b>Apparaatbeheer</b>'</li>
+        <li>OF in 'run' type het commando: <b>devmgmt.msc</b></li>
+        </ul>
+        Of via Fabrikant Tools zoals:
+        <ul>
+        <li>Dell Support Assist</li>
+        <li>HP Support Assistant</li>
+        </ul>
+        Alternatieve Tools:
+        <ul>
+        <li>GPU-Z</li>
+        <li>CPU-Z</li>
+        <li>Unkown Device Identifier</li>
+        <li>Everest home</li>
+        </ul>
+        <b>Controleer</b> de drivers; Vouw elke hoofdcategorie uit zoals <b>Display adapters</b> en <b>Network adapters</b>.<br>
+        Uitleg icoontjes bij de apparaten:
+        <ul>
+        <li>⚠️: Een fout bij de hardware of driver van apparaat. Afkomstig door een ontbrekende driver, resource conflict of een installatie error.</li>
+        <li>⬇️: Het apparaat is manueel uitgeschakeld. Het apparaat zal terug in werking treden na het inschakelen via het context menu.</li>
+        <li>ℹ️: Geeft weer dat de resources manueel werden toegewezen. Het indiceert dat het 'Use automatic settings' onderdeel niet is geselecteerd.</li>
+        <li>❓: Verschijnt wanneer Windows het apparaat detecteert, maar het apparaat niet kan identificeren of de driver ervan installeren. Vereist de juiste driver te installeren.</li>
+        </ul>`],
+    ["Windows Services Configureren", ``]
 ];
 
 let terms = [];
